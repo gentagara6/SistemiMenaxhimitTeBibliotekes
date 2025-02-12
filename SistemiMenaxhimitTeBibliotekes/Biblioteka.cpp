16df642668e74378b00dd072e3dbba4f3973944a
gentagara <gentagara6@gmail.com>
2025-02-12 16:26:19 +0100

    Struktura e librit dhe vargu për ruajtjen e librave

--- SistemiMenaxhimitTeBibliotekes/Biblioteka.cpp
+++ SistemiMenaxhimitTeBibliotekes/Biblioteka.cpp
@@ -3,7 +3,21 @@
 #include <algorithm>
 using namespace std;
 
+#define MAX_LIBRA 100
 
+struct Libri
+{
+	int id = 0;
+    string titulli = " ";
+    string autori = " ";
+    int viti_publikimit = 0;
+    bool ekziston = false;
+    int vleresimi = 0;
+    string kategoria = " ";
+};
+
+Libri biblioteka[MAX_LIBRA];
+int numri_librave;
 
 int main() {
 
